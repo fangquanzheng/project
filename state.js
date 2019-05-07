59b9954e3940414ccb1349f627dec3d0dc9ac0fd
fangquanzheng <47224915+fangquanzheng@users.noreply.github.com>
2019-05-07 06:01:32 -0400

    Update state.js

--- state.js
+++ state.js
@@ -1,20 +1,20 @@
 var dataP = d3.json('us-states.json').then(function(data) {
     drawMap(data);
+    drawHappy(data);
 })
 var dataW = d3.json('wage.json').then(function(data) {
     drawBar(data);
 })
 var drawMap = function(data) {
     var screen = {
-        width: 850,
+        width: 1000,
         height: 650
     };
     var svg = d3.select("#map")
         .attr("width", screen.width)
         .attr("height", screen.height);
     var path = d3.geoPath().projection(d3.geoAlbersUsa());
-    var color = d3.scaleQuantize()
-        .range(d3.schemePuBu[0, 9]);
+  var color = d3.scaleOrdinal().domain([60000,130000]).range(["#ffbd6e", "#ffb761","#ffb154","#feaa47","#fea439","#fe9f30","#fe9927","#fe941c","#fe8f16","#fe890f","#ff8407","#ff7e00"]);
     var features = data.features;
     color.domain([
         d3.min(features, function(d) {
@@ -29,15 +29,16 @@ var drawMap = function(data) {
         .data(data.features)
         .enter()
         .append("path")
-        .attr("transform", "translate(-50,50)")
+        .attr("transform", "translate(50,50)")
         .attr("d", path)
-        .attr("stroke", "black")
+        .attr("stroke", "white")
         .on("mouseover", function(d) {
+
             d3.select("#tooltip")
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY) + "px")
                 .select("#value")
-                .text(d.properties.salary)
+                .text("State: " +d.properties.name + " Salary : " + d.properties.salary)
             d3.select("#tooltip").classed("hidden", false);
         })
         .style("fill", function(d) {
@@ -53,29 +54,211 @@ var drawMap = function(data) {
             d3.select("#tooltip").classed("hidden", true);
         })
         .attr("d", path);
+        var w = 140, h = 300;
+
+var lowColor = '#ffbd6e'
+var highColor = '#ff7e00'
+        var key = d3.select("svg")
+            .append("g")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("class", "legend");
+
+        var legend = key.append("defs")
+            .append("g:linearGradient")
+            .attr("id", "gradient")
+            .attr("x1", "100%")
+            .attr("y1", "0%")
+            .attr("x2", "100%")
+            .attr("y2", "100%")
+            .attr("spreadMethod", "pad");
+
+        legend.append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", highColor)
+            .attr("stop-opacity", 0.9);
+            
+        legend.append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", lowColor)
+            .attr("stop-opacity", 0.9);
+
+        key.append("rect")
+            .attr("width", w - 100)
+            .attr("height", h)
+            .style("fill", "url(#gradient)")
+            .attr("transform", "translate(20,25)");
+
+        var y = d3.scaleLinear()
+            .range([h, 0])
+            .domain([60000, 130000]);
+
+        var yAxis = d3.axisRight(y);
+
+        key.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(60,25)")
+            .attr("opacity",0.7)
+            .call(yAxis)
+}
+var drawHappy = function(data) {
+    var screen = {
+        width: 1000,
+        height: 650
+    };
+    var svg = d3.select("#happy")
+        .attr("width", screen.width)
+        .attr("height", screen.height);
+    var path = d3.geoPath().projection(d3.geoAlbersUsa());
+    var color = d3.scaleOrdinal().domain([30,70]).range(["#ffbd6e", "#ffb761","#ffb154","#feaa47","#fea439","#fe9f30","#fe9927","#fe941c","#fe8f16","#fe890f","#ff8407","#ff7e00"]);
+    var features = data.features;
+    color.domain([
+        d3.min(features, function(d) {
+            return d.properties.happy;
+        }),
+        d3.max(features, function(d) {
+            return d.properties.happy;
+        })
+    ]);
+    //TOOPTIP
+    svg.selectAll("path")
+        .data(data.features)
+        .enter()
+        .append("path")
+        .attr("transform", "translate(50,50)")
+        .attr("d", path)
+        .attr("stroke", "white")
+        .on("mouseover", function(d) {
+
+            d3.select("#tooltip")
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px")
+                .select("#value")
+                .text("State: " +d.properties.name + " Happinese Index: " + d.properties.happy)
+            d3.select("#tooltip").classed("hidden", false);
+        })
+        .style("fill", function(d) {
+            //GET VALUE
+            var index = d.properties.happy;
+            if (index) {
+                return color(index);
+            } else {
+                return "#ccc";
+            }
+        })
+        .on("mouseout", function(d, i) {
+            d3.select("#tooltip").classed("hidden", true);
+        })
+        .attr("d", path);
+        var w = 140, h = 300;
+
+        var lowColor = '#ffbd6e'
+var highColor = '#ff7e00'
+        var key = d3.select("svg")
+            .append("g")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("class", "legend");
+
+        var legend = key.append("defs")
+            .append("g:linearGradient")
+            .attr("id", "gradient")
+            .attr("x1", "100%")
+            .attr("y1", "0%")
+            .attr("x2", "100%")
+            .attr("y2", "100%")
+            .attr("spreadMethod", "pad");
+
+        legend.append("stop")
+            .attr("offset", "0%")
+            .attr("stop-color", highColor)
+            .attr("stop-opacity", 0.9);
+            
+        legend.append("stop")
+            .attr("offset", "100%")
+            .attr("stop-color", lowColor)
+            .attr("stop-opacity", 0.9);
+
+        key.append("rect")
+            .attr("width", w - 100)
+            .attr("height", h)
+            .style("fill", "url(#gradient)")
+            .attr("transform", "translate(20,25)");
+
+        var y = d3.scaleLinear()
+            .range([h, 0])
+            .domain([60000, 130000]);
+
+        var yAxis = d3.axisRight(y);
+
+        key.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(60,25)")
+            .attr("opacity",0.7)
+            .call(yAxis)
+}
+isSalaryMap = 1;
+var changeGeoMap = function() {
+    if (isSalaryMap == 1) {
+        var svg = d3.select("#map")
+            .attr("display", "none");
+        var svg = d3.select("#happy")
+            .attr("display", "block");
+        var svg = d3.select("#bar")
+            .attr("display", "none");
+        isSalaryMap = 2;
+    } else if (isSalaryMap == 2) {
+        var svg = d3.select("#map")
+            .attr("display", "none");
+        var svg = d3.select("#happy")
+            .attr("display", "none");
+        var svg = d3.select("#bar")
+            .attr("display", "block");
+        isSalaryMap = 3;
+    } else {
+        var svg = d3.select("#map")
+            .attr("display", "block");
+        var svg = d3.select("#happy")
+            .attr("display", "none");
+        var svg = d3.select("#bar")
+            .attr("display", "none");
+        isSalaryMap = 1
+    }
 }
 var drawBar = function(data) {
     console.log(data);
-    var screen = {
-        width: 850,
-        height: 650
+    // var screen = {
+    //     width: 850,
+    //     height: 650
+    // }
+    var margin = {
+        top:20,
+        bottom:20,
+        left:30,
+        right:80,
     }
+    var width = 850; 
+    var height = 650;
+    var chartW = width - margin.left - margin.right;
+    var chartH = height - margin.top - margin.bottom;
+    var padding = 5;
     var svg = d3.select("#bar")
-        .attr("width", screen.width)
-        .attr("height", screen.height);
+        .attr("width", width)
+        .attr("height", height);
     var barPadding = 5;
-    var barWidth = (screen.width / dataset.length);
+    var barWidth = (width / data.length);
     var xScale = d3.scaleLinear()
-        .domain([0, 11])
+        .domain([0, data.length])
         .nice()
         .range([0, chartW]);
     var binMaker = d3.histogram()
         .domain(xScale.domain())
-        .thresholds(xScale.ticks(11));
-    var bins = binMaker(quizes);
-    var max = d3.max(bins, function(d) {
-        return d.length;
+        .thresholds(xScale.ticks(data.length));
+    var populations = data.map(function(d) {
+         return d["Total Population"];
     })
+    var bins = binMaker(populations);
+    var max = d3.max(populations);
     var yScale = d3.scaleLinear()
         .domain([0, max])
         .range([chartH, 0])
@@ -91,15 +274,16 @@ var drawBar = function(data) {
         .attr("fill", function(d) {
             return colors(d);
         })
-        .attr("x", function(d) {
-            console.log(d["Total Population"]);
-            return xScale(d.x0) + 50;
+        .attr("x", function(d, i) {
+            //console.log(d["Total Population"]);
+            return i * (width / data.length);
         })
         .attr("y", function(d) {
-            return yScale(d.length) + 100;
+            return yScale(d["Total Population"]);
         })
-        .attr("width", chartW / 11)
+        .attr("width", chartW / data.length)
         .attr("height", function(d) {
-            return chartH - yScale(d.length);
+            console.log(yScale(d["Total Population"]));
+            return chartH - yScale(d["Total Population"]);
         })
 }
